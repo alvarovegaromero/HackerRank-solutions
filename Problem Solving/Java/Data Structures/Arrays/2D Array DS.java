c5fe313d09b9e2784ba27c9e31dc5658dbe6f722
Álvaro <vegaromeroalvaro@gmail.com>
2025-01-01 22:08:08 +0100

    style: use camelCase nomenclature

--- Problem Solving/Java/Data Structures/Arrays/2D Array DS.java
+++ Problem Solving/Java/Data Structures/Arrays/2D Array DS.java
@@ -11,19 +11,19 @@ class Result {
 
      public static int hourglassSum(List<List<Integer>> arr) {        
         int max = Integer.MIN_VALUE;
-        int current_value;
+        int currentValue;
         
         for(int i = 0; i < arr.size()-2 ; i++){ 
             for(int j = 0; j < arr.get(i).size()-2; j++){
-                current_value = 0;
+                currentValue = 0;
 
                 // Upper row elements + Middle's element + Lower row elements
-                current_value += arr.get(i).get(j) + arr.get(i).get(j+1) + arr.get(i).get(j+2) + 
+                currentValue += arr.get(i).get(j) + arr.get(i).get(j+1) + arr.get(i).get(j+2) + 
                                  arr.get(i+1).get(j+1) +
                                  arr.get(i+2).get(j) + arr.get(i+2).get(j+1) + arr.get(i+2).get(j+2);       
                                                 
-                if(current_value > max)
-                    max  = current_value;
+                if(currentValue > max)
+                    max  = currentValue;
             }
         }
         
