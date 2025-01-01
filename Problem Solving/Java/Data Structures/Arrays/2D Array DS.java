965259c5c861b14efb7c8e9cd1e81e39a2c12c53
Álvaro <vegaromeroalvaro@gmail.com>
2025-01-01 20:01:46 +0100

    style: remove empty lines, comments and do the sum in only one statement

--- Problem Solving/Java/Data Structures/Arrays/2D Array DS.java
+++ Problem Solving/Java/Data Structures/Arrays/2D Array DS.java
@@ -9,20 +9,18 @@ class Result {
      * The function accepts 2D_INTEGER_ARRAY arr as parameter.
      */
 
-    public static int hourglassSum(List<List<Integer>> arr) {        
+     public static int hourglassSum(List<List<Integer>> arr) {        
         int max = Integer.MIN_VALUE;
         int current_value;
         
         for(int i = 0; i < arr.size()-2 ; i++){ 
             for(int j = 0; j < arr.get(i).size()-2; j++){
                 current_value = 0;
-                current_value += arr.get(i).get(j) + arr.get(i).get(j+1) 
-                                + arr.get(i).get(j+2); //Upper row elements
-                                                
-                current_value += arr.get(i+1).get(j+1); //Middle's element
-                
-                current_value += arr.get(i+2).get(j) + arr.get(i+2).get(j+1) 
-                                + arr.get(i+2).get(j+2); //Lower row elements            
+
+                // Upper row elements + Middle's element + Lower row elements
+                current_value += arr.get(i).get(j) + arr.get(i).get(j+1) + arr.get(i).get(j+2) + 
+                                 arr.get(i+1).get(j+1) +
+                                 arr.get(i+2).get(j) + arr.get(i+2).get(j+1) + arr.get(i+2).get(j+2);       
                                                 
                 if(current_value > max)
                     max  = current_value;
@@ -31,5 +29,4 @@ class Result {
         
         return max;
     }
-
 }
