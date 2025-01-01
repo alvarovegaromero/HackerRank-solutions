726f9f3c43745cce788f67fc88d0b3ab5c0accef
Álvaro <vegaromeroalvaro@gmail.com>
2025-01-01 19:51:16 +0100

    style: remove comments and empty spaces

--- Problem Solving/Java/Data Structures/Arrays/2D Array DS.java
+++ Problem Solving/Java/Data Structures/Arrays/2D Array DS.java
@@ -9,14 +9,11 @@ class Result {
      * The function accepts 2D_INTEGER_ARRAY arr as parameter.
      */
 
-    public static int hourglassSum(List<List<Integer>> arr) {
-        // Write your code here
-        
+    public static int hourglassSum(List<List<Integer>> arr) {        
         int max = Integer.MIN_VALUE;
         int current_value;
         
         for(int i = 0; i < arr.size()-2 ; i++){ 
-            //System.out.println(" ");
             for(int j = 0; j < arr.get(i).size()-2; j++){
                 current_value = 0;
                 current_value += arr.get(i).get(j) + arr.get(i).get(j+1) 
@@ -26,9 +23,7 @@ class Result {
                 
                 current_value += arr.get(i+2).get(j) + arr.get(i+2).get(j+1) 
                                 + arr.get(i+2).get(j+2); //Lower row elements            
-                                
-                //System.out.print(current_value+" ");    
-                
+                                                
                 if(current_value > max)
                     max  = current_value;
             }
