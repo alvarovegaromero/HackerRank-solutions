0475dffd55498e5255d9f5e653720273e8939d23
Álvaro <vegaromeroalvaro@gmail.com>
2025-01-04 13:44:01 +0100

    feat: Find merge point of two lists problem solved

---
+++ Problem Solving/Java/Data Structures/Linked Lists/Find Merge Point of Two Lists.java
@@ -0,0 +1,34 @@
+//https://www.hackerrank.com/challenges/find-the-merge-point-of-two-joined-linked-lists/problem?
+
+    // Complete the findMergeNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+
+static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        Set<SinglyLinkedListNode> nodes1 = new HashSet<>();
+        SinglyLinkedListNode current = head1;
+        
+        while(current != null){
+            nodes1.add(current);
+            current = current.next;
+        }
+
+        current = head2;
+        
+        while(current != null){
+            if(nodes1.contains(current))
+                return current.data;
+            
+            current = current.next;
+        }    
+        
+        return -1;
+    }
