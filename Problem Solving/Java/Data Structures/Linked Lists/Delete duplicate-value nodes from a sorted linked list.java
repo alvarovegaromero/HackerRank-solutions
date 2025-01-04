2c4aa7844846052e961c7250fde66f1be0467c5b
Álvaro <vegaromeroalvaro@gmail.com>
2025-01-04 12:56:11 +0100

    feat: delete duplicate nodes from a sorted linked list problem solved

---
+++ Problem Solving/Java/Data Structures/Linked Lists/Delete duplicate-value nodes from a sorted linked list.java
@@ -0,0 +1,35 @@
+//https://www.hackerrank.com/challenges/delete-duplicate-value-nodes-from-a-sorted-linked-list/problem
+
+class Result {
+
+    /*
+     * Complete the 'removeDuplicates' function below.
+     *
+     * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+     * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+     */
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+
+    public static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode llist) {
+        SinglyLinkedListNode current = llist;
+
+        while (current != null && current.next != null) {
+            if (current.data == current.next.data) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        
+        return llist;
+    }
+}
