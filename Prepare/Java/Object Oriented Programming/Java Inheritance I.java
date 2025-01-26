1f1418ae1ba4574ac4f1e0c0f95bdf5321a8d07c
Álvaro <vegaromeroalvaro@gmail.com>
2025-01-26 22:10:09 +0100

    feat: Java Inheritance problem solved

---
+++ Prepare/Java/Object Oriented Programming/Java Inheritance I.java
@@ -0,0 +1,29 @@
+//https://www.hackerrank.com/challenges/java-inheritance-1/problem
+
+class Animal{
+	void walk(){
+		System.out.println("I am walking");
+	}
+}
+
+class Bird extends Animal{
+	void fly(){
+		System.out.println("I am flying");
+	}
+    
+    void sing(){
+        System.out.print("I am singing");
+    }
+}
+
+public class Solution{
+
+   public static void main(String args[]){
+
+	  Bird bird = new Bird();
+	  bird.walk();
+	  bird.fly();
+      bird.sing();
+	
+   }
+}
