8271e1d2d11993e1a9cc3e66b0c0836f07aed4ec
Álvaro <vegaromeroalvaro@gmail.com>
2025-01-28 19:03:51 +0100

    feat: problem Java Inheritance II solved

---
+++ Prepare/Java/Object Oriented Programming/Java Inheritance II.java
@@ -0,0 +1,10 @@
+//https://www.hackerrank.com/challenges/java-inheritance-2/problem
+
+class Arithmetic {
+    int add(int a, int b){
+        return a+b;
+    }
+}
+
+class Adder extends Arithmetic {}
+
