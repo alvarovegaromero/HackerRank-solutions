4e155abb94a22a4c23d6ead83e03f31a7bc665f4
Álvaro <vegaromeroalvaro@gmail.com>
2025-03-25 18:02:04 +0100

    feat: "java bigInteger" problem solved

--- solutions/Prepare/Java/BigNumber/Java BigInteger.java
+++ solutions/Prepare/Java/BigNumber/Java BigInteger.java
@@ -1,2 +1,21 @@
 //https://www.hackerrank.com/challenges/java-biginteger/problem
 
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        BigInteger a = sc.nextBigInteger();
+        // another option is BigIngeger a = new BigInteger(sc.next());
+        BigInteger b = sc.nextBigInteger();
+    
+        sc.close();
+        
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
