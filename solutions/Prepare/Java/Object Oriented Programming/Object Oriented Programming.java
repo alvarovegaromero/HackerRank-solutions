1e44b1498cd987066a59699c060484a8b35eb523
Álvaro <vegaromeroalvaro@gmail.com>
2025-05-07 21:38:26 +0200

    feat: "object oriented progamming" problem solved

---
+++ solutions/Prepare/Java/Object Oriented Programming/Object Oriented Programming.java
@@ -0,0 +1,13 @@
+//https://www.hackerrank.com/challenges/java-interface
+
+class  MyCalculator implements AdvancedArithmetic{
+    public int divisor_sum(int n) {
+        int sum = 0; 
+             
+        for (int i = 1 ; i <= n ; i++ )
+            if(n % i == 0)
+                sum += i;
+    
+        return sum;
+    }
+}
