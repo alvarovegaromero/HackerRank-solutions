6c2e281874d0fa79f7538b2ddf104165b36a7a95
Álvaro <vegaromeroalvaro@gmail.com>
2025-05-25 14:47:05 +0200

    feat: "java stdin and stdout 1" problem solved

---
+++ solutions/Prepare/Java/Introduction/Java Stdin and Stdout I.java
@@ -0,0 +1,16 @@
+//https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println(scan.nextInt());
+        System.out.println(scan.nextInt());
+        System.out.println(scan.nextInt());
+        
+        scan.close();
+    }
+}
