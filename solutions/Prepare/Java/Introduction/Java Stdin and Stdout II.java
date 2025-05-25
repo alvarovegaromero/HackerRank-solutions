4ec4040dd0cb847b6f619dc9340a3754cde7a2a1
Álvaro <vegaromeroalvaro@gmail.com>
2025-05-25 14:47:22 +0200

    feat: "java stdin and stdout 2" problem solved

---
+++ solutions/Prepare/Java/Introduction/Java Stdin and Stdout II.java
@@ -0,0 +1,23 @@
+//https://www.hackerrank.com/challenges/java-stdin-stdout/problem
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine(); // consume what's left from current line
+        String s = scan.nextLine();
+        
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+                
+        scan.close();
+    }
+}
